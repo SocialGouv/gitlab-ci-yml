442cd9215d1abb89e515c21b2c37a0c829daa19f
Douglas Duteil <douglasduteil@gmail.com>
2019-12-12 17:21:01 +0100

    Update and rename git_changes.sh to git_files_changes.sh

--- git_files_changes.sh
+++ git_files_changes.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-function did_changes() {
+function git_files_changes() {
   echo "Check if ${1} changes"
   echo "git diff --stat HEAD..origin/${CI_DEFAULT_BRANCH}"
-  git diff --stat HEAD..origin/${CI_DEFAULT_BRANCH}
+  [[ -n "$TRACE" ]] && git diff --stat HEAD..origin/${CI_DEFAULT_BRANCH}
   git diff --stat HEAD..origin/${CI_DEFAULT_BRANCH} | head -n -1 | cut -d' ' -f2 | grep $1
 }
